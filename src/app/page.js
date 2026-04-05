b640fccda61c0aaab54d0cd101366a9eaac40c19
BrandonLeeStudent <bklee2@usf.edu>
2026-04-05 13:50:56 -0400

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,64 +2,41 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <section>
+      <h1 className="text-3xl font-bold mb-4 text-center">Resume</h1>
+      <h1 className="text-2xl font-semibold mb-2 text-center">Brandon Lee</h1>
+      <p>
+        Detail-oriented Analyst experienced in analyzing financial data, preparing forecasts, variance 
+        analyses, and budgeting reports to support business decision-making. Proficient in Microsoft 
+        Excel with experience using Tableau, and SQL for data analysis and financial reporting. 
+        Knowledgeable in financial statements, accounting principles, and corporate finance, with the 
+        ability to monitor financial performance, identify trends, and assess risks. Strong analytical, 
+        problem-solving, and collaboration skills, with experience working with cross-functional teams 
+        to deliver actionable business insights and improve operational efficiency.
+      </p>
+      
+      <h2 className="border-t border-gray-300">EDUCATION</h2>
+      <p>University of South Florida - Tampa, FL Expected graduation: May 2027</p>
+      <p>Bachelor's degree in business Analytics and Information Systems | GPA 3.37</p>
+
+      <h2 className="border-t border-gray-300">EXPERIENCE</h2>
+      <p>Grocery Clerk | May 2024 – Present | Publix - Bradenton, FL</p>
+      <p>Arby's - Lakewood Ranch, FL | March 2022 – April 2023</p>
+
+      <h2>INTERNSHIP EXPERIENCE</h2>
+      <p>Marketing Assistant Summer 2022 | GFree - San Juan, PR</p>
+      <ul>
+        <li>Researched pricing data for ecommerce competition.</li>
+        <li>Proposed price strategy based on gathered data.</li>
+        <li>Fixed product description in the webpage to increase sales.</li>
+      </ul>
+
+      <Image
+        src="/resume jp.jpg"
+        alt="Resume JP"
+        width={500}
+        height={500}
+      />
+    </section>
   );
 }
